b55de203b47856105140894734edd73109d20e74
frankxyy <aaa>
2023-05-10 05:41:09 -0400

    add

---
+++ gen_model_path_in_container.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+
+if __name__ == '__main__':
+    # 检查输入参数是否存在
+    if len(sys.argv) < 2:
+        print("请输入一个路径名作为参数！")
+        sys.exit(1)
+
+    # 获取第一个参数
+    path = sys.argv[1]
+
+    # 获取上一级路径名
+    # parent_path = os.path.dirname(path)
+    if path[-1] == '/':
+        path = path[:-1]
+    segs = path.split('/')
+    res = '/'.join('', 'data', segs[-1])
+    # if path[0] == '/':
+    #     parent_path = '/' + parent_path
+
+    # 输出上一级路径名
+    print(res)
+
+    sys.exit(0)
