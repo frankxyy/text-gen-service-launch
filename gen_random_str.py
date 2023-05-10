34f270e4bdf735317c7d9938891224c09dec9f8c
frankxyy <aaa>
2023-05-10 04:05:22 -0400

    add

--- gen_random_str.py
+++ gen_random_str.py
@@ -6,10 +6,12 @@ def generate_random_string(length):
     letters_and_digits = string.ascii_letters + string.digits
     return ''.join(random.choice(letters_and_digits) for _ in range(length))
 
-# 调用函数生成8位随机字符串
-random_string = generate_random_string(8)
 
-# 打印随机字符串
-print("随机字符串:", random_string)
+if __name__ == '__main__':
+    # 调用函数生成8位随机字符串
+    random_string = generate_random_string(8)
 
-return random_string
+    # 打印随机字符串
+    print("随机字符串:", random_string)
+
+    return random_string
--- path_process.py
+++ path_process.py
@@ -1,18 +1,20 @@
 import os
 import sys
 
-# 检查输入参数是否存在
-if len(sys.argv) < 2:
-    print("请输入一个路径名作为参数！")
-    sys.exit(1)
 
-# 获取第一个参数
-path = sys.argv[1]
+if __name__ == '__main__':
+    # 检查输入参数是否存在
+    if len(sys.argv) < 2:
+        print("请输入一个路径名作为参数！")
+        sys.exit(1)
 
-# 获取上一级路径名
-parent_path = os.path.dirname(path)
+    # 获取第一个参数
+    path = sys.argv[1]
 
-# 输出上一级路径名
-print("上一级路径名：", parent_path)
+    # 获取上一级路径名
+    parent_path = os.path.dirname(path)
 
-return parent_path
+    # 输出上一级路径名
+    print("上一级路径名：", parent_path)
+
+    return parent_path
